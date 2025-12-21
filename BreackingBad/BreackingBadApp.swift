19fd465cc39faa63ab69b7e952d24eb0c145f57d
Yan <yan.yuhtman@gmail.com>
2025-12-21 14:44:52 +0200

    Initial Commit

---
+++ BreackingBad/BreackingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreackingBadApp.swift
+//  BreackingBad
+//
+//  Created by Yan  on 21/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct BreackingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
