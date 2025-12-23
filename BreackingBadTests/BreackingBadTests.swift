92edbf67b1c1e8a762102ee80db93e33dd850ce2
Yan <yan.yuhtman@gmail.com>
2025-12-23 10:34:08 +0200

    Testing git

--- BreackingBadTests/BreackingBadTests.swift
+++ BreackingBadTests/BreackingBadTests.swift
@@ -8,22 +8,26 @@
 import Testing
 
 struct BreackingBadTests {
-
+    
     @Test func fetchQuotasTest() async throws {
-            let result = try await FetchingServise().fetchRandomQuote(production: Production.BreakingBad)
-            let a = result.quote
+        let result = try await FetchingServise().fetchRandomQuote(production: Production.BreakingBad)
+        let a = result.quote
         let t = 0
     }
-
+    
     
     @Test func fetchCharacterTest() async throws {
         let result = try await FetchingServise().fetchCharacter(name: "Jesse Pinkman")
-       
+        
     }
     
     @Test func fetchDeath() async throws {
         let result = try await FetchingServise().fetchDeath(name: "Walter White")
         assert(result != nil)
     }
-
+    
+    @Test func testCommit(){
+        
+    }
+    
 }
